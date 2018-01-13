1f27449af1e8ff65903794cf30bde7d7bd8837a4
test <32431269+zaq12392@users.noreply.github.com>
2018-01-13 13:55:11 +0800

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+package demo;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+  private static Scanner scanner;
+
+  public static void main(String[] args) {
+
+    scanner = new Scanner(System.in);
+
+    Shop[] data = Shop.getShops();
+    while (true) {
+      System.out.println("想吃甚麼?");
+      System.out.println("1.東南亞菜 2.台菜 3.日式料理 4.美式料理 5.義大利料理");
+      int input = scanner.nextInt();
+      int chooseFood = 0;
+      while(true) {
+        System.out.println("轉盤:\n");
+        Random ran = new Random();
+        switch (input) {
+        case 1:
+          System.out.println(data[0].getName() + "\n" + data[1].getName());
+          chooseFood = ran.nextInt(2);// 0-1
+          break;
+        case 2:
+          System.out.println(data[2].getName() + "\n" + data[3].getName());
+          chooseFood = ran.nextInt(2) + 2;// 2-3
+          break;
+        case 3:
+          System.out.println(data[4].getName() + "\n" + data[5].getName());
+          chooseFood = ran.nextInt(2) + 4;// 4-5
+          break;
+        case 4:
+          System.out.println(data[6].getName() + "\n" + data[7].getName());
+          chooseFood = ran.nextInt(2) + 6;// 6-7
+          break;
+        case 5:
+          System.out.println(data[8].getName() + "\n" + data[9].getName());
+          chooseFood = ran.nextInt(2) + 8;// 8-9
+          break;
+        }
+        System.out.println("\n抽選結果:"+ data[chooseFood].getName() + "\n");
+        System.out.println("要詳細資料嗎?");
+        System.out.println("1.要 2.不要");
+        if(scanner.nextInt() == 1) {
+          System.out.print(data[chooseFood]);
+        }
+        System.out.println("這間可以嗎?");
+        System.out.println("1.滿意 2.重新選擇");
+        if(scanner.nextInt() == 1) {
+          break;
+        } else {
+          input += ran.nextInt(3);
+          if(input>5) {
+            input -= 5;
+          }
+        }
+      }
+      
+      System.out.println("1.評分 2.不評分");
+      input = scanner.nextInt();
+      if (input == 1) {
+        System.out.println("請輸入 1.差 2.中 3.好");
+        data[chooseFood].setPoint(scanner.nextInt());
+        System.out.println("評分成功!\n\n");
+      }
+
+    }
+  }
+}
---
+++ src/main/java/Shop.java
@@ -0,0 +1,196 @@
+package demo;
+
+public class Shop{
+    private int point;
+    private String name;
+    private String typeString;
+    private int type;
+    private String location;
+    private int phone;
+
+
+
+  /**
+  * Default empty Shop constructor
+  */
+  public Shop() {
+    super();
+        this.point = -1;
+        this.name = "N/A";
+        this.typeString = "N/A";
+        this.type = 0;
+        this.location = "N/A";
+        this.phone = 0;
+  }
+
+  /**
+  * Default Shop constructor
+  */
+  public Shop(int point, String name, String typeString, int type, String location, int phone) {
+    super();
+    this.point = point;
+    this.name = name;
+    this.typeString = typeString;
+    this.type = type;
+    this.location = location;
+    this.phone = phone;
+  }
+
+  /**
+  * Returns value of point
+  * @return
+  */
+  public int getPoint() {
+    return point;
+  }
+
+  /**
+  * Sets new value of point
+  * @param
+  */
+  public void setPoint(int point) {
+    this.point = point;
+  }
+
+  /**
+  * Returns value of name
+  * @return
+  */
+  public String getName() {
+    return name;
+  }
+
+  /**
+  * Sets new value of name
+  * @param
+  */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+  * Returns value of typeString
+  * @return
+  */
+  public String getTypeString() {
+    return typeString;
+  }
+
+  /**
+  * Sets new value of typeString
+  * @param
+  */
+  public void setTypeString(String typeString) {
+    this.typeString = typeString;
+  }
+
+  /**
+  * Returns value of type
+  * @return
+  */
+  public int getType() {
+    return type;
+  }
+
+  /**
+  * Sets new value of type
+  * @param
+  */
+  public void setType(int type) {
+    this.type = type;
+  }
+
+  /**
+  * Returns value of location
+  * @return
+  */
+  public String getLocation() {
+    return location;
+  }
+
+  /**
+  * Sets new value of location
+  * @param
+  */
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  /**
+  * Returns value of phone
+  * @return
+  */
+  public int getPhone() {
+    return phone;
+  }
+
+  /**
+  * Sets new value of phone
+  * @param
+  */
+  public void setPhone(int phone) {
+    this.phone = phone;
+  }
+
+
+
+  /**
+  * Create string representation of Shop for printing
+  * @return
+  */
+  public String toString() {
+        
+        if(this.point >= 0){
+            String record = "好";
+            if(point == 3) {
+              record = "好";
+            }else if(point == 2) {
+              record = "中";
+            }else {
+              record = "差";
+            }
+            return "商家名字：" + name + "\n" +
+                   "商家類別：" + typeString + "\n" +
+                   "商家位置：" + location + "\n" +
+                   "商家電話：04-" + phone + "\n" +
+                   "商家評分：" + record + "\n\n";
+        }else{
+            return "商家名字：" + name + "\n"+
+                   "商家類別： " + typeString + "\n" +
+                   "商家位置：" + location + "\n" +
+                   "商家電話：04-" + phone + "\n\n";
+                   
+        }
+  }
+  
+ 
+
+
+
+
+    // 0 ： 馬
+    // 1 ： 台
+    // 2 ： 日
+    // 3 ： 美
+    // 4 ： 義
+
+    public static Shop[] getShops(){
+
+        Shop[] Data = new Shop[10];
+
+        Data[0] = new Shop(-1, "南香", "東南亞菜", 0, "逢甲路20巷28弄9號",24521828);
+        Data[1] = new Shop(-1, "瓦城泰國料理", "東南亞菜", 0, "台灣大道三段301號", 22521733);
+        Data[2] = new Shop(-1, "太祖飲食館", "台菜" , 1, "福上巷214弄", 24524563);
+        Data[3] = new Shop(-1, "擄胃專家", "台菜", 1, "文華路127巷22號", 24519878);
+        Data[4] = new Shop(-1, "三次魚屋", "日式料理", 2, "河南路二段255之1號", 24517808);
+        Data[5] = new Shop(-1, "滿燒肉丼食堂", "日式料理", 2, "文華路100號", 24528618);
+        Data[6] = new Shop(-1, "麥味登", "美式料理", 3, "逢大路113號", 24523123);
+        Data[7] = new Shop(-1, "麥當勞", "美式料理", 3, "福星路427號", 24513592);
+        Data[8] = new Shop(-1, "OHLALA", "義大利料理", 4, "文華路179號", 27081200);
+        Data[9] = new Shop(-1, "隨義煮", "義大利料理", 4, "西安街183號", 24523023);
+
+        return Data;
+    }
+
+
+}
